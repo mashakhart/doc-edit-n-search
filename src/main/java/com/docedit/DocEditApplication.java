a13d18cfcb6abcdde2ee8581f99583fc47acbc25
Maria Khartchenko <84724262+mashakhart@users.noreply.github.com>
2026-08-06 23:47:24 -0400

    basic UI added

--- src/main/java/com/docedit/DocEditApplication.java
+++ src/main/java/com/docedit/DocEditApplication.java
@@ -1,12 +1,21 @@
 package com.docedit;
 
+import com.docedit.store.DocumentStore;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DocEditApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(DocEditApplication.class, args);
+    }
+
+    /** Seeds an empty "Untitled" document so a freshly started app always has one to open. */
+    @Bean
+    CommandLineRunner seedUntitledDocument(final DocumentStore store) {
+        return args -> store.create("", "Untitled");
     }
 }
